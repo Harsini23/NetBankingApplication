55046d7a0edcf0a19cb886528f4ff5f2f4597c8b
Harsini SR <harsini.ramalingam@gmail.com>
2023-02-19 22:48:05 +0530

    login changes

---
+++ NetBankingApplication/ViewModel/DispatcherExtensionMethod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+
+namespace NetBankingApplication.ViewModel
+{
+    public static class DispatcherExtensionMethod
+    {
+        public static async Task RunAsync(this CoreDispatcher dispatcher, Action action)
+        {
+            await dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => action());
+        }
+    }
+}
