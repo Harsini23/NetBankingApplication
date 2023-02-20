f9d7d88be65fc455ca2374e7d0a277e5aab34398
Harsini SR <harsini.ramalingam@gmail.com>
2023-02-20 18:50:36 +0530

    minor changes

--- NetBankingApplication/View/UserControls/Overview.xaml.cs
+++ NetBankingApplication/View/UserControls/Overview.xaml.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Input;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -58,5 +59,10 @@ namespace NetBankingApplication.View.UserControls
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RPBTest_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+          
+        }
     }
 }
