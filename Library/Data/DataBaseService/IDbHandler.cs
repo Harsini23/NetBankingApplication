1ff665f915b5039ce1a4f79c629e2f9603b6a89c
Harsini SR <harsini.ramalingam@gmail.com>
2023-03-27 12:57:53 +0530

    minor fix

--- Library/Data/DataBaseService/IDbHandler.cs
+++ Library/Data/DataBaseService/IDbHandler.cs
@@ -7,51 +7,83 @@ using System.Threading.Tasks;
 
 namespace Library.Data.DataBaseService
 {
-    public interface IDbHandler
+    public interface IDbHandler: IUserDbHandler, ICredentialDbHandler, IAccountDbHandler, IAdminDbHandler, ITransactiondbHandler, IPayeeDbHandler, IUserAccountDbHandler,IBranchDbHandler
     {
+     
+       
+    }
+    public interface INetHandler
+    {
+
+    }
+    public interface IUserDbHandler
+    {
+        List<User> GetAllUsers();
         bool CheckUser(string userId);
         User GetUser(string userId);
         void BlockAccount(string userId);
         void UnBlockAccount(string userId);
         void AddUser(User user);
+        string GetUserName(String userId);
+        bool UpdateUser(User updatedUser);
+        bool IfUserAlreadyExists(string email, long mobileNo, string Pan);
+        string GetProfile(string userId);
+    }
+
+    public interface ICredentialDbHandler
+    {
         bool CheckIfUserExists(string userId);
-        bool CheckIfAdminExists(string userId);
         bool CheckUserCredential(string userId, string password);
         bool CheckIfAdmin(string userId);
         bool CheckIfNewUser(string userId);
         bool ResetPassword(Credentials newCredential);
-        bool AddTransaction(Transaction transaction);
+        void CreateCredential(Credentials cred);
+        void CreateDefaultAdminIfNotExists(Credentials credentials);
+
+    }
+
+    public interface IAccountDbHandler
+    {
         void AddAccount(Account account);
-        List<Transaction> GetAllTransactions(string userId,bool fetchRecentTransactions);
-        bool AddNewPayee(Payee newPayee);
-        void AddAccountForUser(UserAccounts userAccounts);
-        List<Payee> GetAllPayee(string userId);
         Account GetAccount(string accountNumber);
         bool UpdateBalance(Account account);
-        void DeletePayee(Payee payee);
-        List<UserAccounts> GetAllAccountsForUser(string userId);
-        List<Transaction> GetTransactionsForAccount(string accountNumber);
-        string GetUserName(String userId);
-        void CreateCredential(Credentials cred);
-        Branch GetBranchDetails(String BId);
         double GetTotalBalanceOfUser(string userId);
         Dictionary<String, double> GetAllAccountBalance(string userId);
-        void CreateDefaultAdminIfNotExists(Credentials credentials);
+    }
+
+    public interface IAdminDbHandler
+    {
+        bool CheckIfAdminExists(string userId);
+    }
+
+    public interface ITransactiondbHandler
+    {
+        bool AddTransaction(Transaction transaction);
+        List<Transaction> GetAllTransactions(string userId, bool fetchRecentTransactions);
+        List<Transaction> GetTransactionsForAccount(string accountNumber);
         double GetTotalIncome(string userId);
         double GetTotalExpense(string userId);
         List<Transaction> GetCurrentMonthIncome(string userId);
         List<Transaction> GetCurrentMonthExpense(string userId);
+
+    }
+    public interface IPayeeDbHandler
+    {
+        bool AddNewPayee(Payee newPayee);
+        List<Payee> GetAllPayee(string userId);
+        void DeletePayee(Payee payee);
+        bool EditPayee(Payee payee);
+    }
+    public interface IUserAccountDbHandler
+    {
+        void AddAccountForUser(UserAccounts userAccounts);
+        List<UserAccounts> GetAllAccountsForUser(string userId);
+    }
+    public interface IBranchDbHandler
+    {
+        Branch GetBranchDetails(String BId);
         void InsertBankBranchDetails(List<Branch> branches);
         List<Branch> GetAllBranches();
-        bool EditPayee(Payee payee);
-        bool UpdateUser(User updatedUser);
-        bool IfUserAlreadyExists(string email,long mobileNo,string Pan);
-        List<User> GetAllUsers();
-        string GetProfile(string userId);
     }
 
-    public interface INetHandler
-    {
-
-    }
 }
---
+++ Library/Util/EmailValidation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Util
+{
+    internal static class EmailValidation
+    {
+        public static bool ValidateEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                return false;
+            }
+
+            try
+            {
+                // Use the MailAddress class to validate the email address (syntax validation)
+                MailAddress addr = new MailAddress(email);
+
+                // Check if the domain name is valid ( split into various sub and top level domain)
+                string[] parts = addr.Host.Split('.');
+                if (parts.Length >= 2)
+                {
+                    //validating top level domain
+                    string tld = parts[parts.Length - 1];
+                    if (tld.Length > 1 && tld.Length <= 4)
+                    {
+                        return true;
+                    }
+                }
+            }
+            catch (FormatException)
+            {
+                // The email address is not valid
+            }
+
+            return false;
+        }
+    }
+}
